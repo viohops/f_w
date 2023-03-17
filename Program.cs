cd18df58e76db1035d41885f3673367973707f60
unknown <vitalii.programm@gmail.com>
2023-03-17 17:57:28 +0300

    ///

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GivenArray = new string[] { "123", "23", "hello", "world", "res", "checklength", "=)" };
+
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ElementsQuantity(string[] array, int lengthLimit)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= lengthLimit) result++;
+    }
+    return result;
+}
+
+int lengthLimit = GetNumber("Введите длину-ограничитель элемента массива");
+int ShortArrayLength = ElementsQuantity(GivenArray, lengthLimit);
